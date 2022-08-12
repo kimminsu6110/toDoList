061b96d18f689c919337bd8375c297806f67c0f7
manzu <kimminsu6110@nate.com>
2022-08-12 23:06:22 +0900

    secondCommit

---
+++ src/routes/Coins.tsx
@@ -0,0 +1,75 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  padding: 0px 20px;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  dispaly: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CoinList = styled.ul``;
+
+const Coin = styled.li`
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  border-radius: 15px;
+  margin-bottom: 10px;
+`;
+const coins = [
+  {
+    id: 'btc-bitcoin',
+    name: 'Bitcoin',
+    symbol: 'BTC',
+    rank: 1,
+    is_new: false,
+    is_active: true,
+    type: 'coin',
+  },
+  {
+    id: 'eth-ethereum',
+    name: 'Ethereum',
+    symbol: 'ETH',
+    rank: 2,
+    is_new: false,
+    is_active: true,
+    type: 'coin',
+  },
+  {
+    id: 'usdt-tether',
+    name: 'Tether',
+    symbol: 'USDT',
+    rank: 3,
+    is_new: false,
+    is_active: true,
+    type: 'token',
+  },
+];
+
+const Title = styled.h1`
+  font-size: 49px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+function Coins() {
+  return (
+    <Container>
+      <Header>
+        <Title>Coins</Title>
+      </Header>
+      <CoinList>
+        {coins.map((coin) => (
+          <Coin key={coin.id}>
+            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+          </Coin>
+        ))}
+      </CoinList>
+    </Container>
+  );
+}
+
+export default Coins;
---
+++ src/theme.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+  bgColor: 'black',
+  textColor: '#f5f6fa',
+  accentColor: '#4cd137',
+};
